3c392df87a6c2ab6a3bed6ed96579967fafd7b72
Rohit Nikam <rnikam18@gmail.com>
2024-03-04 20:38:39 +0530

    changed the code

--- resource/storage_account/main.tf
+++ resource/storage_account/main.tf
@@ -1,13 +1,3 @@
-terraform{
-    backend "aws_s3_bucket" {
-        region = "us-west-2"
-        bucket = "terraform-202412"
-        key = "dev.aws_s3_bucket.tfstate"
-        encrypt = true
-        dynamodb_table = "terraform-202412"  
-    }
-}
-
 #Create S3 bucket
 resource "aws_s3_bucket" "rjnstorage0019" {
   bucket = "rjnstorage0019"
