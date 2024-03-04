696f007a3627ce85c8d56568c856b61539c838b4
Rohit Nikam <rnikam18@gmail.com>
2024-03-04 20:36:54 +0530

    changed the code

--- resource/storage_account/provider.tf
+++ resource/storage_account/provider.tf
@@ -1,16 +1,14 @@
 terraform {
   required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.92.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "azurerm" {
-  subscription_id = "f89c9fbd-eb95-45b1-b08a-ac68effe3c93"
-  tenant_id = "3385756d-d0cd-480a-b29b-dd3a76f458db"
-  client_id = "dfa1a378-1edc-4e71-b9be-4949c4a0dd28"
-  client_secret = "ODC8Q~osnQ6t5buhaOvnrWzeHOB7LxhxYFqsna2o"
-  features {}  
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
+
