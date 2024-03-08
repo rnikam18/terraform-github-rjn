8ead85c7cec987f29cae59ea1383b2248153d8a8
Rohit Nikam <rnikam18@gmail.com>
2024-03-08 18:12:17 +0530

    added new resource aks

--- resource/storage_account/provider.tf
+++ resource/storage_account/provider.tf
@@ -6,3 +6,25 @@ terraform {
     }
   }
 }
+
+resource "azurerm_resource_group" "rjn" {
+  name= "rjn-rg"
+  location= "Central India"
+}
+
+resource "azurerm_storage_account" "rjnstorage" {
+  name                     = "rjnstorage"
+  resource_group_name      = azurerm_resource_group.rjn.name
+  location                 = azurerm_resource_group.rjn.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "rjn-rg"
+    storage_account_name  = "rjnstorage"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
